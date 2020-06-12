bd4766927509ca9d3ee75c93f5bcce61a7e4ca49
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-12 17:00:36 +0430

    primary delete api

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -9,13 +9,23 @@ router.post("/", async (req, res) => {
   const { error } = validateTask(req.body);
   if (error) return res.status(404).send(error.details[0].message);
   const newTask = {
-    id: tasks.length+1,
+    id: tasks.length + 1,
     title: req.body.title,
     desc: req.body.desc,
     status: req.body.status,
   };
   tasks.push(newTask);
   res.status(200).send(newTask);
+});
+
+router.delete("/:id", async (req, res) => {
+  const task = tasks.find((x) => x.id == req.params.id);
+  if (!task) return res.status(404).send();
+  tasks.splice(
+    tasks.findIndex((x) => x == task),
+    1
+  );
+  res.status(200).send();
 });
 
 module.exports = router;

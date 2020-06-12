a003f54856410d2e3c256f19ae8ee913dccf0147
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-12 16:13:59 +0430

    adding primary validation

--- src/models/task.js
+++ src/models/task.js
@@ -1,9 +1,21 @@
+const Joi = require('joi')
+
 const tasks = [{ id: 1, title: "Task 1", desc: "design api", status: 2 },
 { id: 2, title: "Task 2", desc: "design rect", status: 1 },
 { id: 3, title: "Task 3", desc: "design db", status: 1 },
-{ id: 2, title: "Task 4", desc: "learn node beginer courses", status: 3 }
+{ id: 4, title: "Task 4", desc: "learn node beginer courses", status: 3 }
 ];
 
-module.exports = tasks;
+const validate = (task) =>
+{
+    const schema = {
+        title: Joi.string().min(3).max(50).required(),
+        desc: Joi.string().required(),
+        status: Joi.number().required()
+    }
+    return Joi.validate(task,schema);
+}
+
+module.exports = {tasks : tasks , validate : validate}
 
 

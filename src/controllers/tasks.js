64b21348dd0e98aa0597648be30cd0a7bd028105
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-12 16:14:18 +0430

    adding primary post

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const tasks = require('../models/task')
+const { tasks, validate: validateTask } = require("../models/task");
 const router = express.Router();
 router.get("/", async (req, res) => {
-  res.send(
-    tasks
-  );
+  res.send(tasks);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validateTask(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+  const newTask = {
+    id: tasks.length+1,
+    title: req.body.title,
+    desc: req.body.desc,
+    status: req.body.status,
+  };
+  tasks.push(newTask);
+  res.status(200).send(newTask);
 });
 
 module.exports = router;

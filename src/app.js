db7f262ceedfaedb1645af7118db3ee8facea8c8
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-14 00:46:39 +0430

    formating code

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const tasks = require('./controllers/tasks.js');
-const home = require('./controllers/home.js');
-const corsPolicy = require('./middleware/cors')
-const mongoose = require('mongoose');
+const express = require("express");
+const tasks = require("./controllers/tasks.js");
+const home = require("./controllers/home.js");
+const corsPolicy = require("./middleware/cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 app.use(corsPolicy);
 
-app.use('/api/tasks/' , tasks);
-app.get('/' , home)
+app.use("/api/tasks/", tasks);
+app.get("/", home);
 
-mongoose.connect('mongodb://localhost/todo').then(() => console.log('Connected To Database..')).catch(err => console.log('Connecting to Database failed..' , err));
+mongoose
+  .connect("mongodb://localhost/todo")
+  .then(() => console.log("Connected To Database.."))
+  .catch((err) => console.log("Connecting to Database failed..", err));
 
 const port = process.env.PORT || 8080;
-app.listen(port , () => console.log(`server started on ${port}`) );
+app.listen(port, () => console.log(`server started on ${port}`));

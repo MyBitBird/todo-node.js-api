4a56f47864d940dbaa898e4f705529722237f893
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-13 19:16:25 +0430

    renaming status to type

--- src/models/task.js
+++ src/models/task.js
@@ -1,9 +1,9 @@
 const Joi = require('joi')
 
-const tasks = [{ id: 1, title: "Task 1", desc: "design api", status: 2 },
-{ id: 2, title: "Task 2", desc: "design rect", status: 1 },
-{ id: 3, title: "Task 3", desc: "design db", status: 1 },
-{ id: 4, title: "Task 4", desc: "learn node beginer courses", status: 3 }
+const tasks = [{ id: 1, title: "Task 1", desc: "design api", type: 2 },
+{ id: 2, title: "Task 2", desc: "design rect", type: 1 },
+{ id: 3, title: "Task 3", desc: "watch videos from Udemy, then larn how to configu mongo db or sqllite and then use it in your project.", type: 1 },
+{ id: 4, title: "Task 4", desc: "learn node beginer courses", type: 3 }
 ];
 
 const validate = (task) =>
@@ -11,7 +11,7 @@ const validate = (task) =>
     const schema = {
         title: Joi.string().min(3).max(50).required(),
         desc: Joi.string().required(),
-        status: Joi.number().required()
+        type: Joi.number().required()
     }
     return Joi.validate(task,schema);
 }

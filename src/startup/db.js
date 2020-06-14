b8daebc490d4dca25a26095603b99811b3ca562d
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-14 23:15:15 +0430

    config package doesnt work on heroku

--- src/startup/db.js
+++ src/startup/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = () => {
-  const mongoUrl = config.get("mongoUrl");
-  console.log('mongo' , mongoUrl);
+  const mongoUrl = process.env.MANGO_URI || "mongodb://localhost/todo";
+ 
   mongoose
     .connect(mongoUrl, { useNewUrlParser: true , useUnifiedTopology: true })
     .then(() => console.log(`Connected To ${mongoUrl}..`))

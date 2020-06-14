7cd8c1ae16e6e1f23e4c8a80751b34d8cc28249b
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-14 22:41:51 +0430

    adding useUnifiedTopology to db connect

--- src/startup/db.js
+++ src/startup/db.js
@@ -3,8 +3,9 @@ const config = require("config");
 
 module.exports = () => {
   const mongoUrl = config.get("mongoUrl");
+  console.log('mongo' , mongoUrl);
   mongoose
-    .connect(mongoUrl, { useNewUrlParser: true })
+    .connect(mongoUrl, { useNewUrlParser: true , useUnifiedTopology: true })
     .then(() => console.log(`Connected To ${mongoUrl}..`))
     .catch((err) => console.log("Connecting to Database failed.. ", err));
 };

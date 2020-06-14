fdcdbe43e38def9ad0f2ea8d69f81342e8ec649f
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-14 17:37:04 +0430

    for deploying

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
 require('./startup/routes')(app);
 require('./startup/db')();
+
+app.use(helmet());
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`server started on ${port}`));

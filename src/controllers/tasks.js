79186220cb75da4f752796c1ae657800ada06204
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-14 13:00:15 +0430

    bug fix - returning 400 instead of 404 for bad requests

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
-  if (error) return res.status(404).send(error.details[0].message);
+  if (error) return res.status(400).send(error.details[0].message);
 
   let newTask = {
     title: req.body.title,
